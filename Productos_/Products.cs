b48b027abfda2dc4e5bb657ad157a579a9415e73
Perla <Perla@DURAN>
2024-11-15 06:45:55 -0400

    Agregar archivos de proyecto.

---
+++ Productos_/Products.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productos
+{
+    public class Productos
+    {
+        public string Nombre { get; set; }
+        public bool seccion { get; set; }
+        public double Precio { get; set; }
+        public int Cantidad { get; set; }
+
+
+        public virtual void Imprimir()
+        {
+            Console.WriteLine($"Nombre{Nombre}");
+            Console.WriteLine($"Precio{Precio}");
+            Console.WriteLine($"Cantidad{Cantidad}");
+        }
+    }
+    public class Papeles : Productos
+    {
+        public string Tipo { get; set; }
+        public string Color { get; set; }
+        public string NombreP { get; set; }
+        public double PrecioP { get; set; }
+        public Papeles(string nombreP, double precioP)
+        {
+            NombreP = nombreP;
+            PrecioP = precioP;
+        }
+        public override void Imprimir()
+        {
+            base.Imprimir();
+            Console.WriteLine($"Tipo de Papel: {Tipo}");
+            Console.WriteLine($"Color del Papel: {Color}");
+        }
+
+
+    }
+    class Utensilio : Productos
+    {
+        public string Material { get; set; }
+
+        public string NombreU { get; set; }
+        public double PrecioU { get; set; }
+
+        public Utensilio(string nombreU, double precioU)
+        {
+            NombreU = nombreU;
+            PrecioU = precioU;
+        }
+        public override void Imprimir()
+        {
+            base.Imprimir();
+            Console.WriteLine($"Material: {Material}");
+        }
+
+
+    }
+}
---
+++ Productos_/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Productos;
+
+namespace Productos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Productos productos = new Productos();
+            Console.WriteLine("¿El producto está en la sección de utensilios? ");
+            Console.WriteLine("Sí es así ingrese ¨true¨ sí no ¨false¨");
+            productos.seccion = Boolean.Parse(Console.ReadLine());
+
+            if (productos.seccion == true)
+            {
+                Console.WriteLine("El producto es de la sección de utensilios");
+                Console.WriteLine("¿Cuantos utensilio desea comprar?");
+                int cantidadU = int.Parse(Console.ReadLine());
+                Utensilio[] utensilio = new Utensilio[cantidadU];
+
+                for (int i = 0; i < cantidadU; i++)
+                {
+                    Console.WriteLine($"Ingrese el nombre del utensilio {i + 1}:");
+                    string nombre = Console.ReadLine();
+                    Console.WriteLine($"Ingrese el precio del utensilio {i + 1}:");
+                    double precio = double.Parse(Console.ReadLine());
+                    utensilio[i] = new Utensilio(nombre, precio);
+                }
+
+                Console.WriteLine("Listado de utensilios ingresados:");
+                foreach (var utensilios in utensilio)
+                {
+                    Console.WriteLine($"Nombre: {utensilios.NombreU}, Precio: {utensilios.PrecioU}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El producto es de la sección de Papeles");
+                Console.WriteLine("¿Cuantos Papeles desea comprar?");
+                int cantidadP = int.Parse(Console.ReadLine());
+                Papeles[] papeles = new Papeles[cantidadP];
+
+                for (int i = 0; i < cantidadP; i++)
+                {
+                    Console.WriteLine($"Ingrese el nombre del utensilio {i + 1}:");
+                    string nombre = Console.ReadLine();
+                    Console.WriteLine($"Ingrese el precio del utensilio {i + 1}:");
+                    double precio = double.Parse(Console.ReadLine());
+                    papeles[i] = new Papeles(nombre, precio);
+                }
+
+                Console.WriteLine("Listado de Papeles ingresados:");
+                foreach (var papel in papeles)
+                {
+                    Console.WriteLine($"Nombre: {papel.NombreP}, Precio: {papel.PrecioP}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
